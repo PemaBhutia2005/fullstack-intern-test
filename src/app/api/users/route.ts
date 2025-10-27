8dc02b39a7507451e8d1b73c52bbf7c8dbde7f07
PemaBhutia2005 <pema.bhutia@student.ehb.be>
2025-10-27 13:59:36 +0100

    Refactor user route to retrieve username from query parameters

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,7 +4,15 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
 
 export async function GET(request: Request) {
-	const username = "demoUser"; // placeholder
+	const url = new URL(request.url);
+	const username = url.searchParams.get("username"); // get username from query
+
+	// check if username is provided
+	if (!username) {
+		return NextResponse.json({ error: "Username required" }, { status: 400 });
+	}
+
+	// check if uri is provided
 	if (!uri) return NextResponse.json({ error: "No URI found" }, { status: 500 });
 
 	const client = new MongoClient(uri);
@@ -18,11 +26,7 @@ export async function GET(request: Request) {
 		const user = await usersCollection.findOne({ _id: username });
 
 		// check if user exists, if not return a default user with empty favorites
-		if (user) {
-			return NextResponse.json(user);
-		} else {
-			return NextResponse.json({ _id: username, favorites: [] });
-		}
+		return NextResponse.json(user || { _id: username, favorites: [] });
 	} catch (error) {
 		return NextResponse.json({ error: "Failed to fetch user" }, { status: 500 });
 	} finally {
@@ -31,23 +35,33 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-	const username = "demoUser"; // placeholder
-	if (!uri) return NextResponse.json({ error: "No URI found" }, { status: 500 });
-
-	const client = new MongoClient(uri);
 	const body = await request.json();
-	const favorites = body.favorites || [];
+	const username = body.username;
+
+	if (!username) {
+		return NextResponse.json({ error: "Username required" }, { status: 400 });
+	}
+
+	if (!uri) return NextResponse.json({ error: "No URI found" }, { status: 500 });
+	const client = new MongoClient(uri);
 
 	try {
 		await client.connect();
 		const db = client.db("skynow");
 		const usersCollection = db.collection("users");
 
-		//create new user
-		await usersCollection.insertOne({
-			_id: username,
-			favorites: favorites,
-		});
+		//check if user exists
+		const userExists = await usersCollection.findOne({ _id: username });
+		if (userExists) {
+			return NextResponse.json({ error: "User already exists" }, { status: 400 });
+		} else {
+			//if not, create new user with empty favorites
+			await usersCollection.insertOne({
+				_id: username,
+				favorites: [],
+			});
+		}
+
 		return NextResponse.json({ message: "User created" });
 	} catch (error) {
 		return NextResponse.json({ error: "Failed to create user" }, { status: 500 });
